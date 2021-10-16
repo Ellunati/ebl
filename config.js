2177efce7e6f9ab0573f42825dc4e5b9c6775e42
Ellunati <76846111+Ellunati@users.noreply.github.com>
2021-10-16 18:15:11 +0300

    31
    31

---
+++ config.js
@@ -0,0 +1,165 @@
+
+        module.exports = {
+            bot: { 
+                prefix: "r!",
+                owners: ["844244033525514280"],
+                mongourl: "mongodb+srv://Rcodes2021:t9jJRtHvWuwo6spc@rcodes2021.qrwrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",  //https://mongodb.com/cloud/atlas/register
+                servers: {
+                    prefix: "r!!"
+                }
+            },
+        
+            website: {
+                callback: "https://Topggv2.npgop1.repl.co/callback", //example : https://vcodes.xyz avoid / at last. 
+                secret: "d4cLEaOBMVHtN-9DXH4gRFDscE_qRVBT",
+                clientID: "884767278099484712", // Bot client id.
+                tags: [ "Moderation", "Fun", "Minecraft","Economy","Guard","NSFW","Anime","Invite","Music","Logging", "Web Dashboard", "Reddit", "Youtube", "Twitch", "Crypto", "Leveling", "Game", "Roleplay", "Utility", "Turkish" ],
+                languages: [
+                    { flag: 'gb', code: 'en', name: 'English' },
+                    { flag: 'tr', code: 'tr', name: 'Türkçe' },
+                    { flag: 'de', code: 'de', name: 'Deutsch' }
+                ],
+                servers: {
+                    tags: [
+                    {
+                        icon: "fal fa-code",
+                        name: "Development"
+                    },
+                    {
+                        icon: "fal fa-play",
+                        name: "Stream"
+                    },
+                    {
+                        icon: "fal fa-camera",
+                        name: "Media"
+                    },
+                    {
+                        icon: 'fal fa-building',
+                        name: 'Company'
+                    },
+                    {
+                        icon: 'fal fa-gamepad',
+                        name: 'Game'
+                    },
+                    {
+                        icon: 'fal fa-icons',
+                        name: 'Emoji'
+                    },
+                    {
+                        icon: 'fal fa-robot',
+                        name: 'Bot List'
+                    },
+                    {
+                        icon: 'fal fa-server',
+                        name: 'Server List'
+                    },
+                    {
+                        icon: 'fal fa-moon-stars',
+                        name: 'Turkish'
+                    },
+                    {
+                        icon: 'fab fa-discord',
+                        name: 'Support'
+                    },
+                    {
+                        icon: 'fal fa-volume',
+                        name: 'Sound'
+                    },
+                    {
+                        icon: 'fal fa-comments',
+                        name: 'Chatting'
+                    },
+                    {
+                        icon: 'fal fa-lips',
+                        name: 'NSFW'
+                    },
+                    {
+                      icon: "fal fa-comment-slash",
+                      name: "Challange"
+                    },
+                    {
+                      icon: "fal fa-hand-rock",
+                      name: "Protest"
+                    },
+                    {
+                      icon: "fal fa-headphones-alt",
+                      name: "Roleplay"
+                    },
+                    {
+                      icon: "fal fa-grin-alt",
+                      name: "Meme"
+                    },
+                    {
+                      icon: "fal fa-shopping-cart",
+                      name: "Shop"
+                    },
+                    {
+                      icon: "fal fa-desktop",
+                      name: "Technology"
+                    },
+                    {
+                      icon: "fal fa-laugh",
+                      name: "Fun"
+                    },
+                    {
+                      icon: "fal fa-share-alt",
+                      name: "Social"
+                    },
+                    {
+                      icon: "fal fa-laptop",
+                      name: "E-Spor"
+                    },
+                    {
+                      icon: 'fal fa-palette',
+                      name: 'Design'
+                    },
+                    {
+                      icon: 'fal fa-users',
+                      name: 'Community'
+                    }
+                    ]                
+                }
+            },
+        
+            server: {
+                id: "897550572062900224",
+                invite: "https://discord.io/rcodes",
+                roles: {
+                    administrator: "897550572125847619",//for rn botlist working server will work on this part 2 video
+                    moderator: "897550572125847617", //server also worked
+                    profile: {
+                        sitecreator : "897550572142604400", //you have to fill it for admin access channel
+                        booster: "",
+                        sponsor: "897550572083896386",
+                        supporter: "898937972395753492",
+                        partnerRole: "897550572062900232"
+                    },
+                    codeshare: {
+                        javascript: "897550572109066314",
+                        html: "898938212372865114",
+                        substructure: "897550572109066313",
+                        bdfd: "897550572109066315", // Bot Designer For Discord
+                        fiveInvite: "897550572109066316",
+                        tenInvite: "897550572109066317",
+                        fifteenInvite: "897550572109066318",
+                        twentyInvite: "897550572109066319"
+                    },
+                    botlist: {
+                        developer: "897550572083896385",
+                        certified_developer: "897550572083896387",
+                        bot: "897550572071301133",
+                        certified_bot: "897550572062900230",
+                    }
+                },
+                channels: {
+                    codelog: "897550573488988160",
+                    login: "897550573488988163",
+                    webstatus: "897550573275062358",
+                    uptimelog: "897550573275062358",
+                    botlog: "897550573275062358",
+                    votes: "897550573275062358"
+                }
+            }
+        
+        
+        }
